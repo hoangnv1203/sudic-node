86845b5925313ef0ce3d8a54586c2fbfd2d90944
hoangnv1203 <hoang.nong.nv@gmail.com>
2018-03-11 15:48:10 +0700

    Add cluster for speeding up app

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import cluster from 'cluster'
+import os from 'os'
 import express from 'express'
 import morgan from 'morgan'
 import config from './config/config'
@@ -7,19 +9,33 @@ import {
   updateBalanceAccount
 } from 'modules/blockchain'
 
-drive.connect().then((ch) => {
-  drive.publish(config.channel.reqCreateWallet, 'Hello Hoang')
+if (cluster.isMaster) {
+  const numWorkers = os.cpus().length
+  console.log('numWorkers', numWorkers)
 
-  subscribeCreateWallet()
-  updateBalanceAccount()
-  drive.subscribe(config.channel.resCreateWallet, (message) => {
-    console.log('message', message);
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal)
+    console.log('Starting a new worker')
+    cluster.fork()
   })
-})
 
+} else {
+  const system = express()
 
-const system = express()
+  drive.connect().then((ch) => {
+    drive.publish(config.channel.reqCreateWallet, 'Hello Hoang')
 
-system.use(morgan('dev'))
+    subscribeCreateWallet()
+    updateBalanceAccount()
+    drive.subscribe(config.channel.resCreateWallet, (message) => {
+      console.log('message', message);
+    })
+  })
 
-system.listen(config.serverPort, () => console.log(`Server listen to :${config.serverPort}`))
+  system.use(morgan('dev'))
+  system.listen(config.serverPort, () => console.log(`Server listen to :${config.serverPort}`))
+}
